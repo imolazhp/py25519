b709b0e8033d384b745e0074d03d185bdfcaea29
Sundar Nagarajan <sun.nagarajan@gmail.com>
2016-03-19 10:30:29 -0700

    Protected py25519/build/build.sh from accidental execution"

--- py25519/build/build.sh
+++ py25519/build/build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+if [ -z "$1" ]; then
+	echo "This file is intended to used only during package installation"
+	exit 0
+fi
 cd $(dirname $0)/../c/
 make libs
 cd ..
 \cp -rf c/. ../$1/c/.
+
+chmod -x $0 1>/dev/null 2>&1
