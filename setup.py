49a04d87ec72851b440f18f1587f1b847f16b114
Sundar Nagarajan <sun.nagarajan@gmail.com>
2016-03-25 16:57:59 -0700

    0.13.6

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ PACKAGE DATA
 '''
 # You _SHOULD_ set these
 name = 'py25519'
-version = '0.13.5'   # oldver: '0.13.4'
+version = '0.13.6'   # oldver: '0.13.5'
 url = 'https://github.com/sundarnagarajan/py25519'
 download_url = '%s/tree/%s' % (url, version)
 description = name
